82bcfc5397cfa91912679dc4484dd126d06ff95f
ruthcastillo7 <castillohuamaniruthm@gmail.com>
2024-05-08 09:05:34 -0500

    cree mi archivo de ejercicios.py el dia de hoy 8 de mayo del 2024

--- controles_flujo/ejercicios.py
+++ controles_flujo/ejercicios.py
@@ -1,15 +1,17 @@
 #escribir un programa que pregunte al usuario su edad y muestre por pantalla
 #si es mayor de edad o no.
-ingrese_edad:int=int(input"ingrese edad usuario: ")
+ingrese_edad:int=int(input("ingrese edad usuario: "))
 if ingrese_edad >= 18:
     print("eres mayor de edad")
 if ingrese_edad < 18:
     print("eres menor de edad")
 #escribir un programa que almacene la cadena de caracteres contraseña en una variable,
-#pregunte al usuario por la contraseña e imprima por pantalla si la contraseña
-#introducida por el usuario coincide con la guardad en la variable sin tener en cuenta mayusculas y minusculas.
-
+#pregunte al usuario por la contraseña e
+#imprima por pantalla si la contraseña introducida por el usuario coincide con la guardad en la variable
+#sin tener en cuenta mayusculas y minusculas.
+contraseña:str=input("ingrese contraseña: ")
+print(id(contraseña))
 #escribir un programa que pida al usuario un numero entero positivo y muestre por
 #pantalla la cuenta atras desde ese numero hasta cero separados por comas.
 usuario:int=int(input("ingrese numero positivo: "))
-print([:usuario])
+#print([:usuario])
