fc1d9fd52df1efb2ae28d3ea5bef0fe228e34b00
ruthcastillo7 <castillohuamaniruthm@gmail.com>
2024-05-23 11:34:17 -0500

    actualice mi carpeta proyecto el dia de hoy 23 de mayo del 2024

--- controles_flujo/proyecto/main.py
+++ controles_flujo/proyecto/main.py
@@ -7,31 +7,35 @@ HORARIOS DISPONIBLES:
 3. MIERCOLES s/. 60: de 4 p.m a 6 p.m
 4. JUEVES s/. 30: de 4 p.m a 5 p.m
 5. VIERNES s/. 30: de 4 p.m a 5 p.m
-6. SABADO s/. 90: de 3 p.m a 3 p.m
+6. SABADO s/. 90: de 3 p.m a 6 p.m
  """
+
 while True:
     print(reserva)
     opcion=input("Ingrese la opcion del horario que desea reservar: ")
     if opcion == "1":
-        print("su recerva fue exitosa, pague s/. 30")
+        print("su recerva del dia LUNES fue exitosa🎉⚽🏀🏐")
     elif opcion == "2":
-        print("su recerva fue exitosa, pague s/. 30")
+        print("su recerva del dia MARTES fue exitosa🎉⚽🏀🏐")
     elif opcion == "3":
-        print("su recerva fue exitosa, pague s/. 60")
+        print("su recerva del dia MIERCOLES fue exitosa🎉⚽🏀🏐")
     elif opcion == "4":
-        print("su recerva fue exitosa, pague s/. 30")
+        print("su recerva del dia JUEVES fue exitosa🎉⚽🏀🏐")
     elif opcion == "5":
-        print("su recerva fue exitosa, pague s/. 30")
+        print("su recerva del dia VIERNES fue exitosa🎉⚽🏀🏐")
     elif opcion == "6":
-        print("su recerva fue exitosa, pague s/. 90")
-        break
-
-# PRECIOS=f"""
-#  1. el precio para el dia lunes es: s/. 30
-#  2. el precio para el dia martes es: s/. 30
-#  3. el precio para el dia miercoles es: s/. 60
-#  4. el precio para el dia jueves es: s/. 30
-#  5. el precio para el dia viernes es: s/. 30
-#  6. el precio para el dia sabado es: s/. 90"""
-# # print(f"""VERIFICACION:
-#       SU RESERVA ES DEL DIA {OPCION} ASI UE PAGUE {PRECIO}""")
+        print("su recerva del dia SABADO fue exitosa🎉⚽🏀🏐")
+    break
+print("VERIFICACION:")
+if opcion == "1":
+        print("su recerva es para el dia LUNES a horas 4 p.m a 5 p.m y debe pagar S/. 30")
+if opcion == "2":
+        print("su recerva es para el dia MARTES a horas 4 p.m a 5 p.m y debe pagar S/. 30")
+if opcion == "3":
+        print("su recerva es para el dia MIERCOLES a horas 4 p.m a 6 p.m y debe pagar S/. 60")
+if opcion == "4":
+        print("su recerva es para el dia MIERCOLES a horas 4 p.m a 5 p.m y debe pagar S/. 30")
+if opcion == "5":
+        print("su recerva es para el dia MIERCOLES a horas 4 p.m a 5 p.m y debe pagar S/. 30")
+if opcion == "6":
+        print("su recerva es para el dia MIERCOLES a horas 3 p.m a 6 p.m y debe pagar S/. 90")
