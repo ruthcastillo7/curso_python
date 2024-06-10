67d58af518503a0f99ee00532c47e057134cb719
ruthcastillo7 <castillohuamaniruthm@gmail.com>
2024-06-10 10:32:35 -0500

    subo mi carpeta funciones hoy dia

--- funciones/lecture.py
+++ funciones/lecture.py
@@ -1,6 +1,7 @@
 # return devuelve valores que podre hacer uso
 ## crear una funcion que retorne el numero 10, y muestra por pantalla si es par
 ### siempre que el valor que retorne mi funcion se utilice en mas sentencias u operaciones 
+# de return
 def diez():
     return 10
 if diez()%2==0:
