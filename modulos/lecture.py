1836d1c0951d051e90c4113ec2227acf84b4b48f
ruthcastillo7 <castillohuamaniruthm@gmail.com>
2024-07-12 10:25:12 -0500

    subo mi carpeta modulos

---
+++ modulos/lecture.py
@@ -0,0 +1,17 @@
+# """precticando pylint"""
+# nombre_alumno:str="miguel"
+
+# def suma(a:int,b:int)->int:
+#     """funcion para sumar dos numeros"""
+#     return a+b
+# print(suma(4,7))
+
+# def resta(a,b):
+#     """funcion para restar dos numeros"""
+#     return a-b
+
+# IMPORTACION DIRECTA
+import operaciones
+suma:int=operaciones.suma(7,8)
+msj:str=f"{operaciones.mensaje},como estas"
+print(suma)
