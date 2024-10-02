2270f7fad545fbf75672124de4d238adcc11acbd
ruthcastillo7 <castillohuamaniruthm@gmail.com>
2024-10-02 16:35:29 -0500

    subiendo mi carpeta POO

--- poo/homework.py
+++ poo/homework.py
@@ -3,25 +3,25 @@
 # sus atributos seran nombre, apllidos, dni, numero de cuenta y saldo inicial
 # como metodos podemos hacer deposito retirar dinero y ver estado de cuenta
 
-# class Banco:
-#     def __init__(self,nombre,apellido,dni,numero_cuenta,saldo_inicial):
-#         self.nombre=nombre
-#         self.apellido=apellido
-#         self.dni=dni
-#         self.numero_cuenta=numero_cuenta
-#         self.saldo_inicial=saldo_inicial
-#         # metodos
-#     def deposita(self,monto):
-#         print("esta depositando: S/. ", monto)
-#     def retirar(self,monto):
-#         print("esta retirando: S/. ",monto)
-#     def ver(self):
-#         print("ve su estado de cuenta")
+class Banco:
+    def __init__(self,nombre,apellido,dni,numero_cuenta,saldo_inicial):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.dni=dni
+        self.numero_cuenta=numero_cuenta
+        self.saldo_inicial=saldo_inicial
+        # metodos
+    def deposita(self,monto):
+        print("esta depositando: S/. ", monto)
+    def retirar(self,monto):
+        print("esta retirando: S/. ",monto)
+    def ver(self):
+        print("ve su estado de cuenta")
 
-# ruth=Banco("ruth","castillo",60414454,2324-2342-3334,100)
-# ruth.deposita(200)
-# ruth.retirar(90)
-# ruth.ver()
+ruth=Banco("ruth","castillo",60414454,2324-2342-3334,100)
+ruth.deposita(200)
+ruth.retirar(90)
+ruth.ver()
 
 # EJERCICIO 2
 # Crear una clase agencia
@@ -44,8 +44,8 @@ class Agencia:
         print("viaje cancelado")
     def estado_viaje(self):
         print("ver su estado de viaje: ")
-
-ruth=Agencia("ruth","castillo",60414454,5,2/10/24)
+#ruth=Agencia("ruth","castillo",60414454,5,2/10/24)
+ruth=Agencia()
 ruth.estado_viaje()
 ruth.origen("puquio")
 ruth.destino("arequipa")
